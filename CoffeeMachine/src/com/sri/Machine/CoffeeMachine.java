d53b467aa814d96e17b3263c8716fa48016ff94d
Srinath <gopathisrinath@gmail.com>
2020-06-29 08:20:35 +0530

    Basic instructions are completed

---
+++ CoffeeMachine/src/com/sri/Machine/CoffeeMachine.java
@@ -0,0 +1,17 @@
+package com.sri.Machine;
+
+public class CoffeeMachine {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+        System.out.println("Starting to make a Coffee");
+        System.out.println("Grinding Coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+    
+	}
+
+}
