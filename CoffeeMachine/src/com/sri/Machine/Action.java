f0d5407a5217fd7618db4a4a5c4d761f50e67e62
Srinath <gopathisrinath@gmail.com>
2020-07-06 12:41:48 +0530

    Actions are completed

---
+++ CoffeeMachine/src/com/sri/Machine/Action.java
@@ -0,0 +1,5 @@
+package com.sri.Machine;
+
+public class Action {
+
+}
--- CoffeeMachine/src/com/sri/Machine/CoffeeMachine.java
+++ CoffeeMachine/src/com/sri/Machine/CoffeeMachine.java
@@ -6,44 +6,37 @@ public class CoffeeMachine {
 	
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Hello World!");
-//        System.out.println("Starting to make a Coffee");
-//        System.out.println("Grinding Coffee beans");
-//        System.out.println("Boiling water");
-//        System.out.println("Mixing boiled water with crushed coffee beans");
-//        System.out.println("Pouring coffee into the cup");
-//        System.out.println("Pouring some milk into the cup");
-//        System.out.println("Coffee is ready!");
-//        System.out.println("Write how many cups of coffee you will need:");
-//        int num = scanner.nextInt();
-//        System.out.println("For " + num + " of coffee you will need:");
-//        System.out.println(num*200 + " ml of water");
-//        System.out.println(num*50 + " ml of milk");
-//        System.out.println(num*15 + " g of coffee beans");
+        
         System.out.println("Write how many ml of water the coffee machine has:");
         int water = scanner.nextInt();
+
         System.out.println("Write how many ml of milk the coffee machine has:");
-         int milk = scanner.nextInt();
-         System.out.println("Write how many grams of coffee beans the coffee machine has:");
-         int beans = scanner.nextInt();
-         System.out.println("Write how many cups of coffee you will need");
-         int cups = scanner.nextInt();
-         
-         int waterCups = water / 200;
-         int milkCups = milk / 50;
-         int beansCups = beans / 15;
+        int milk = scanner.nextInt();
 
-         int limit = Math.min(waterCups, Math.min(milkCups, beansCups));
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = scanner.nextInt();
 
-         if (cups > limit) {
-             System.out.println("No, I can make only " + limit + " cup(s) of coffee");
-         } else if (cups == limit) {
-             System.out.println("Yes, I can make that amount of coffee");
-         } else {
-             int extra = limit - cups;
-             System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
-         }
-         
+        System.out.println("Write how many cups of coffee you will need:");
+        int n = scanner.nextInt();
+        
+ 
+        water = water/200;
+        milk = milk/ 50;
+        beans = beans/ 15;
+
+        int min = Math.min(water, milk);
+        System.out.println(min);
+        min = Math.min(beans, min);
+        System.out.println(min);
+
+        if (n == min) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (n < min) {
+            System.out.println("Yes, I can make that amount of coffee (and even " +
+                    (min - n) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + min + " cup(s) of coffee");
+        }
          
          
 	}
